7504eebfe7399d3ba2df45a7375104a567a8538f
RohitKaushal7 <rohitkk252550@gmail.com>
2019-02-10 01:54:28 +0530

    pop up

--- main.php
+++ main.php
@@ -6,6 +6,24 @@
 	<link rel="stylesheet" type="text/css" href="../../bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../../fontawesome-free-5.6.3-web\css/all.min.css">
 	<script src="../../jquery.min.js"></script>
+
+	<?php
+		$con = mysqli_connect('localhost','root','');
+		mysqli_select_db($con,'hack3');
+
+		$qre = "select * from products";
+
+		$res = mysqli_query($con,$qre);
+
+
+
+
+	?>
+
+
+
+
+
 </head>
 <body>
 
@@ -32,44 +50,46 @@
 
 		<div class="item-box">
 			
-			<div class="item">
-				<div class="details"></div>
+		<!-- 	<div class="item">
+				<div class="details">
+					<h4>Sony SRS 10</h4>
+					<p>Bluetooth speakers</p>
+					<div class="price">&#8377; 2000</div>
+				</div>
+				<i class="fa fa-cart-plus fa-2x"></i>
+			</div>
+ -->
+
+			<?php
+				if(mysqli_num_rows($res)==0)
+				{
+			?>
+
+				<blockquote><h1>Nothing Here</h1></blockquote>
+
+			<?php }  else { 
+
+				while($row = mysqli_fetch_assoc($res))
+				{
+
+				?>
+
+				<div class="item" style="background:url(./pics/<?php echo $row['ppic']?>); background-size: cover;">
+				<div class="details">
+					<h4> <?php echo $row['pname'] ?> </h4>
+					<p> <?php echo $row['pdetails'] ?> </p>
+					<div class="price">&#8377; <?php echo $row['pprice'] ?> </div>
+				</div>
+				<i class="fa fa-cart-plus fa-2x"></i>
 			</div>
 
 
-			<div class="item">
-				<div class="details"></div>
-			</div>
+			<?php }  }?>
 
 
-			<div class="item">
-				<div class="details"></div>
-			</div>
 
-
-			<div class="item">
-				<div class="details"></div>
-			</div>
-
-
-			<div class="item">
-				<div class="details"></div>
-			</div>
-
-
-			<div class="item">
-				<div class="details"></div>
-			</div>
-
-
-			<div class="item">
-				<div class="details"></div>
-			</div>
 
 			
-			<div class="item">
-				<div class="details"></div>
-			</div>
 
 		</div>
 	</div>
@@ -86,6 +106,50 @@
 	</footer>
 
 </div>
+
+
+<script type="text/javascript">
+		
+	$(function(){
+
+		$(".fa-cart-plus").click(function(){
+			if($(this).parent().css("position")=="relative"){
+					$(".item").css("filter","blur(2px)");
+					$(this).parent().css({
+						"position":"fixed",
+						"z-index":"3",
+
+						"top":"30%",
+						"left":"20%",
+						"transform": "scale(2)",
+						"filter":"blur(0px)"
+					});
+			}
+
+			else
+			{
+				$(".item").css("filter","blur(2px)");
+					$(this).parent().css({
+						"position":"relative",
+						"z-index":"1",
+						"top":"0",
+						"left":"0",
+						"transform": "scale(1)",
+						"filter":"blur(0px)"
+					});
+
+					$(".item").css("filter","blur(0px)");
+
+			}
+
+		});
+
+	});
+
+</script>
+
+
+
 
 </body>
 </html>
